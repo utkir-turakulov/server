6f3699561912af8f7202e4bcac4f15fc40d0423e
BarrySpidster <silverhawk1195@gmail.com>
2018-02-02 00:52:00 +0400

    настройка

--- start.js
+++ start.js
@@ -1,4 +1,31 @@
-let express = require('express'),
+let http = require('http');
+let express = require('express');
+let app = express();
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let express = require('express'),
     winston = require('winston'),
 path = require('path');
 const port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -38,4 +65,4 @@ app.get('/images/:file',function(req,res){
 });
 
 app.listen(port,ip_address);
-console.log("Слушаем хост:" + ip_address + " порт: " + port);
+console.log("Слушаем хост:" + ip_address + " порт: " + port);*/
